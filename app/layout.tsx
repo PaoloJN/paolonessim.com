39a67fc5f2947748eaf16f041dd3664d1f91e573
Paolo Nessim <paolo.j8n@gmail.com>
2024-04-24 18:34:23 -0400

    Add @vercel/analytics package and include analytics component in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           </div>
           {/* <TailwindIndicator /> */}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
