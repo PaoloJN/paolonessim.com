8d368d3765e00fd78a4ed1b2b8408b876ccbad64
Paolo Nessim <paolo.j8n@gmail.com>
2024-06-25 18:35:06 -0400

    chore: Update Next.js configuration to include CORS headers for API routes

--- next.config.mjs
+++ next.config.mjs
@@ -19,19 +19,19 @@ const nextConfig = {
   async headers() {
     return [
       {
+        // matching all API routes
         source: "/api/:path*",
         headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // Set your origin
-          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
           },
           {
             key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
+            value:
+              "Sec-WebSocket-Accept, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
