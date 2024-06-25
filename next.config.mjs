2ac546700b1e3a7406c02fcb3ded2a5e098913df
Paolo Nessim <paolo.j8n@gmail.com>
2024-06-25 18:15:48 -0400

    chore: Add CORS headers for API routes

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,27 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
